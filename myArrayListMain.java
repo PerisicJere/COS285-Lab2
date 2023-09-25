60ccfbd9d34c678698aaed299032112ad8db69cb
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2023-09-25 01:08:00 -0400

    DONE

--- myArrayListMain.java
+++ myArrayListMain.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 /*
  * CLASS FractionMain
@@ -8,30 +9,41 @@ import java.util.Arrays;
  * @version 20230917
  */
 public class myArrayListMain {
-/*
- *@author Jere Perisic
- * Testing myArrayList.java
- *
- */
+	/*
+	 *@author Jere Perisic
+	 * Testing myArrayList.java
+	 *
+	 */
 	public static void main(String[] args) {
 
-	testAdd();
-	testIndexOf();
+		testAdd();
+		testIndexOf();
+		testRemove();
+		testCapacity(61);
+
 
 	}
-	public static void testAdd(){
+
+	/*
+	 * Tests the add method of myArrayList.
+	 */
+	public static void testAdd() {
 		System.out.println("Testing add method.");
 		myArrayList list = new myArrayList();
 		for (int i = 0; i < 10; i++) {
 			Fraction fraction = new Fraction(i, i + 2);
 			list.add(list.getSize(), fraction);
-			System.out.print(list.get(i)+", ");
+			System.out.print(list.get(i) + ", ");
 		}
-		System.out.println("\n"+"Add successful");
-		System.out.println(list.getCapacity());
+		System.out.println("\n" + "Add successful");
+		System.out.println(list.getCapacity() + "\n");
 	}
-	public static void testIndexOf(){
-		System.out.println("Test: IndexOf");
+
+	/*
+	 * Tests the indexOf method of myArrayList.
+	 */
+	public static void testIndexOf() {
+		System.out.println("Testing IndexOf");
 		myArrayList list = new myArrayList();
 
 		Fraction fra1 = new Fraction(1, 2);
@@ -39,10 +51,46 @@ public class myArrayListMain {
 		list.add(0, fra1);
 		list.add(1, fra2);
 		int index = list.indexOf(fra2);
-		System.out.println("Fra2: "+index);
+		System.out.println("Fra2: " + index);
 		int nonExistingIndex = list.indexOf(new Fraction(2, 3));
-		System.out.println("Non existing index: "+nonExistingIndex);
-		System.out.println("Index of testing done");
+		System.out.println("Non existing index: " + nonExistingIndex);
+		System.out.println("Index of testing done" + "\n");
 	}
 
+	/*
+	 * Tests the remove method of myArrayList.
+	 */
+	public static void testRemove() {
+		System.out.println("Testing remove");
+		myArrayList list = new myArrayList();
+
+		Fraction fra1 = new Fraction(1, 2);
+		Fraction fra2 = new Fraction(3, 4);
+
+		list.add(0, fra1);
+		list.add(1, fra2);
+
+		Fraction removed = list.remove(0);
+
+		assert removed.equals(fra1);
+
+		assert list.getSize() == 1;
+	}
+	/*
+	 * Tests the capacity increase of myArrayList.
+	 *
+	 * @param n The number of fractions to add for testing capacity increase.
+	 */
+
+	public static void testCapacity(int n) {
+		System.out.println("Testing capacity increase");
+		myArrayList list = new myArrayList();
+
+		System.out.println("Initial capacity:" + list.getCapacity());
+		for (int i = 0; i < n; i++) {
+			list.add(i, new Fraction(i, i + 1));
+		}
+		System.out.println("Increased capacity: " + list.getCapacity());
+
+	}
 }
